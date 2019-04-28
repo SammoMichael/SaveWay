f1bd7325065a1a00b79035399e142cb236a7ba51
SammoMichael <sammomichael@gmail.com>
2019-04-27 22:36:12 -0400

    style login

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Button, TouchableOpacity, Platform, StyleSheet, TextInput, Text, View, Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,10 +20,51 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <View backgroundColor="green" style={styles.container}>
+        <Image
+          style={{ width: 300, height: 200 }}
+          source={require('./img/logo.png')}
+          />
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
+        <TextInput 
+          style={styles.textfield}
+          editable
+          backgroundColor="white"
+          placeholder="Username"
+          // borderRadius={15}
+          height={5 + '%'}
+          width={80 + '%'}
+          marginBottom={10 + '%'}
+        />
+        <TextInput 
+          style={styles.textfield}
+          editable
+          backgroundColor="white"
+          // borderRadius={15}
+          placeholder="Password"
+          height={5 + '%'}
+          width={80 + '%'}
+          marginBottom={10 + '%'}
+
+          // padding={}
+        />
+        <TouchableOpacity style={styles.underline} ><Text color="white">Forgot Username/Password?</Text></TouchableOpacity>
+        {/* <Button color="#841584" title="Login"></Button>
+        <Button color="#841584" title="Sign Up"></Button> */}
+        <TouchableOpacity
+          style={styles.loginScreenButton}
+          // onPress={() => navigate('HomeScreen')}
+          underlayColor='#fff'>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.signupScreenButton}
+          // onPress={() => navigate('HomeScreen')}
+          underlayColor='#fff'>
+          <Text style={styles.loginText}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -34,12 +75,62 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  loginScreenButton: {
+
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#5F8318',
+    borderRadius: 15,
+    borderWidth: 1,
+    // borderColor: '#fff'
+  },
+  signupScreenButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#01171B',
+    borderRadius: 15,
+    borderWidth: 1,
+    // borderColor: '#fff'
+  },
+  loginText: {
+    color: 'white',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  underline: {
+    textDecorationLine: 'underline',
+    color: 'white',
+    paddingBottom: 30,
+  },
+  textfield: {
+    fontSize: 20,
+    height: 50,
+    // width: 100 + "%",
+    marginHorizontal: 50,
+    // backgroundColor: "rgb(255,255,255)",
+    marginBottom: 10,
+    paddingBottom: 30,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
   },
   instructions: {
     textAlign: 'center',
---
+++ App2.js
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import Spotogram from './src/Spotogram.js';
+// import PostFeed from './src/components/container/PostFeed';
+
+// export default class App extends Component {
+//     render() {
+//         return (
+//             <View >
+//                 <Spotogram />
+//                 {/* <PostFeed />  */}
+
+//             </View>
+
+//         );
+//         // return (
+//         //   < View >
+//         //     <Spotogram />
+
+//         //     <PostFeed />
+//         //   </View>
+//         // );
+//     }
+// }
